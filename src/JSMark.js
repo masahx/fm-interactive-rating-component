e363407a40d17b21ee33a103d4e6dadf43d88d91
masha <spamovski@yahoo.co.uk>
2023-09-25 12:33:09 +0200

    Added HTML5 semantic tags instead of divs

--- src/JSMark.js
+++ src/JSMark.js
@@ -1,111 +1,111 @@
-  const class1 = "mark";
-  const class2 = "circle";
+const class1 = "mark";
+const class2 = "circle";
 
-  let oLista = document.getElementsByTagName("ol")[0];
+let oLista = document.getElementsByTagName("ol")[0];
   
-  const ALL_OFF = -1;  
+const ALL_OFF = -1;  
 
-  class Marks {
+class Marks {
     
-    currentOn = ALL_OFF;
-    #marksNumber;
+  currentOn = ALL_OFF;
+  #marksNumber;
     
-    constructor() {
+  constructor() {
       
-      this.#marksNumber = 5;   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes/Private_class_fields 
+    this.#marksNumber = 5;   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes/Private_class_fields 
+
+  }
+
+  toggleMarkColor(ii) {
+
+    var listt = oLista.querySelectorAll("li");
+    if (this.currentOn >= 0) {
+
+      listt[this.currentOn].classList.remove("chosen-mark");
 
     }
 
-    toggleMarkColor(ii) {
+    if (this.currentOn != ii) {
 
-      var listt = oLista.querySelectorAll("li");
-      if (this.currentOn >= 0) {
+      listt[ii].classList.add("chosen-mark");
+      this.currentOn = ii;
 
-        listt[this.currentOn].classList.remove("chosen-mark");
+    } else {
 
-      }
-
-      if (this.currentOn != ii) {
-
-        listt[ii].classList.add("chosen-mark");
-        this.currentOn = ii;
-
-      } else {
-
-        this.currentOn = ALL_OFF;
-
-      }
-
-    }
-
-    makeButtons() {
-  
-      let myLi;
-      for (let i = 0; i < this.#marksNumber; i++) {
-        
-        myLi = document.createElement("li");
-        myLi.classList.add(class1, class2);
-        myLi.innerHTML = i + 1;     
-        myLi.setAttribute("onclick", "m.toggleMarkColor(" + i + ");");
-        oLista.appendChild(myLi);
-
-      }
-
-    }
-
-    getMarksNumber() {
-
-      return this.#marksNumber;
+      this.currentOn = ALL_OFF;
 
     }
 
   }
+
+  makeButtons() {
+  
+    let myLi;
+    for (let i = 0; i < this.#marksNumber; i++) {
+        
+      myLi = document.createElement("li");
+      myLi.classList.add(class1, class2);
+      myLi.innerHTML = i + 1;     
+      myLi.setAttribute("onclick", "m.toggleMarkColor(" + i + ");");
+      oLista.appendChild(myLi);
+
+    }
+
+  }
+
+  getMarksNumber() {
+
+    return this.#marksNumber;
+
+  }
+
+}
    
-  var m = new Marks();
-  m.makeButtons();  
+var m = new Marks();
+m.makeButtons();  
 
-  testMode(false);
+testMode(false);
 
-  function testMode(ttest) {
+function testMode(ttest) {
 
-    const turnOffClass = "off";
-    var classes = document.querySelector(".back").classList;
+  const turnOffClass = "off";
+  var classes = document.querySelector(".back").classList;
     
-    if (ttest) {
+  if (ttest) {
 
-      classes.remove(turnOffClass);
+    classes.remove(turnOffClass);
   
-    } else if (!classes.contains("off")) {
+  } else if (!classes.contains("off")) {
 
-      classes.add(turnOffClass);
+    classes.add(turnOffClass);
         
-    }
-
   }
 
-  function submit() {
+}
+
+function submit() {
     
-    if (m.currentOn != -1) {
+  if (m.currentOn != -1) {
   
-      document.querySelector(".resolved-mark").innerHTML = m.currentOn + 1;
-      document.querySelector("#thanks").classList.remove("off");  
-      document.querySelector(".rating").classList.add("off");
+    document.querySelector(".resolved-mark").innerHTML = m.currentOn + 1;
+    document.querySelector("#thanks").classList.remove("off");  
+    document.querySelector(".rating").classList.add("off");
          
-    }
-
   }
 
+}
 
-  function goBack() {
 
-    var firstCardList = document.querySelector(".rating").classList;
-    document.querySelector("#thanks").classList.add("off");
+function goBack() {
 
-    if (firstCardList.contains("off")) {
+  var firstCardList = document.querySelector(".rating").classList;
+  document.querySelector("#thanks").classList.add("off");
 
-      m.toggleMarkColor(m.currentOn);
-      firstCardList.remove("off");
+  if (firstCardList.contains("off")) {
 
-    }        
+    m.toggleMarkColor(m.currentOn);
+    firstCardList.remove("off");
 
-  }
+  }        
+
+}
